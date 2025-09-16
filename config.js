ebd0bf00d23803eef00cd39790fb6910a455b6ad
AdielsonMedeiros <medeirosadielson@gmail.com>
2025-09-16 00:45:37 -0300

    remove gitignore

---
+++ config.js
@@ -0,0 +1,11 @@
+// Arquivo: config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC54qbzx3YQJ2gam0LNC0KvRD4CIxzFLR8",
+  authDomain: "gastos-b24a4.firebaseapp.com",
+  projectId: "gastos-b24a4",
+  storageBucket: "gastos-b24a4.appspot.com",
+  messagingSenderId: "456706783032",
+  appId: "1:456706783032:web:365ce9cb1f61c3c9dd73cd",
+  measurementId: "G-H391E5P236"
+};
